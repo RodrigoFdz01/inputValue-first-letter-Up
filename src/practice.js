76174ea4728b48444a033c4a50755eced6554ad6
Rodrigo Fernandez <rodrigof3012@gmail.com>
2022-08-16 17:04:01 -0300

    Initial commit

---
+++ src/practice.js
@@ -0,0 +1,45 @@
+let input = document.getElementById("input");
+
+let button = document.getElementById("button");
+let newarray = [];
+
+button.addEventListener("click", function () {
+  let valueInput = input.value;
+  //console.log(typeof valueInput);
+  const words = valueInput.split(" ");
+  //console.log(words); // ['asd', 'asd', 'asd']
+
+  let words2 = words
+    .map((word) => {
+      return word[0].toUpperCase() + word.substring(1);
+      //console.log(words);
+    })
+    .join(" ");
+  newarray.push(words2);
+  console.log(newarray);
+});
+/*
+// Primer letra de array de nombres a mayuscula
+let actores = [
+  "tom hanks",
+  "johnny deep",
+  "samuel l jackson",
+  "Morgan Freeman",
+];
+let newarray = [];
+
+function toUPer(array) {
+  for (let i = 0; i < array.length; i++) {
+    const words = array[i].split(" ");
+    let wordsMaped = words
+      .map((word) => {
+        return word[0].toUpperCase() + word.substring(1);
+      })
+      .join(" ");
+    newarray.push(wordsMaped);
+  }
+  return newarray;
+}
+
+toUPer(actores);
+*/
